a613eb80ddd1798130d5cac86c1e22e40d7e2a5b
Emma Haruka Iwao <yuryu@yuryu.net>
2026-06-28 07:39:35 -0700

    Initial Commit

---
+++ IPAKeyboard/IPAKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPAKeyboardApp.swift
+//  IPAKeyboard
+//
+//  Created by Emma Haruka Iwao on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct IPAKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
